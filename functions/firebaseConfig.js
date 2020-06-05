854db1d763af487e40c623a86c544ef46bfcfdce
Antony Prince <47826916+CodeDecoder01@users.noreply.github.com>
2020-06-05 15:35:12 +0530

    Update firebaseConfig.js

--- functions/firebaseConfig.js
+++ functions/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBqyZtabwYKZemHCSPXP9cO6WdA7wmVFOo",
-    authDomain: "elitedoctorsconsultancy-3291a.firebaseapp.com",
-    databaseURL: "https://elitedoctorsconsultancy-3291a.firebaseio.com",
+    apiKey: "<Enter api key>",
+    authDomain: "<Enter your value>",
+    databaseURL: "<Enter your value>",
     projectId: "elitedoctorsconsultancy-3291a",
-    storageBucket: "elitedoctorsconsultancy-3291a.appspot.com",
-    messagingSenderId: "1021452637022",
-    appId: "1:1021452637022:web:a284a33ec6f77e7e7c75cf",
-    measurementId: "G-YDFPLDSEFP"
+    storageBucket: "<Enter your value>",
+    messagingSenderId: "<Enter your value>",
+    appId: "<Enter your value>",
+    measurementId: "<Enter your value>"
   };
 module.exports=firebaseConfig;
